d6e251278cdda3918e2062e81385cc85264e3335
Lucas Lima <id.lucas@outlook.com.br>
2022-10-29 20:31:12 -0300

    Environment Variables configuration

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -9,4 +9,15 @@ const firebaseConfig = {
   appId: process.env.APP_ID
 };
 
+console.log (
+  {
+    apiKey: process.env.API_KEY,
+    authDomain: process.env.AUTH_DOMAIN,
+    projectId: process.env.PROJECT_ID,
+    storageBucket: process.env.STORAGE_BUCKET,
+    messagingSenderId: process.env.MESSAGING_SENDER_ID,
+    appId: process.env.APP_ID
+  }
+)
+
 export const firebase = initializeApp(firebaseConfig);
