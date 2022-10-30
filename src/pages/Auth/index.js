8c4ea8d177978391d9d33f7e800c250b5fe7c178
Lucas Lima <id.lucas@outlook.com.br>
2022-10-30 12:23:17 -0300

    remove auth facebook

--- src/pages/Auth/index.js
+++ src/pages/Auth/index.js
@@ -1,39 +1,16 @@
 import { Navigate } from 'react-router-dom';
 import { useAuthGoogle } from '../../hooks/useAuthGoogle';
 
-import { getAuth, signInWithPopup, FacebookAuthProvider } from "firebase/auth";
-
 
 import * as S from './styles';
 
 import Google from '../../assets/icons/google.webp';
-import { firebase } from '../../config/firebase';
 
 function Auth() {
   const { signInGoogle, signed } = useAuthGoogle();
-  const provider = new FacebookAuthProvider();
 
   async function loginGoogle() {
     await signInGoogle();
-  }
-
-  const loginFacebook = () => {
-
-    const auth = getAuth(firebase);
-    signInWithPopup(auth, provider)
-      .then((result) => {
-        const user = result.user;
-        const credential = FacebookAuthProvider.credentialFromResult(result);
-        const accessToken = credential.accessToken;
-
-        console.log(user, accessToken)
-      })
-      .catch((error) => {
-        // const errorCode = error.code;
-        // const errorMessage = error.message;
-        // const email = error.customData.email;
-        // const credential = FacebookAuthProvider.credentialFromError(error);
-      });
   }
 
   if (!signed) {
@@ -42,10 +19,6 @@ function Auth() {
         <button onClick={loginGoogle}>
           <img src={Google} alt="" />
           <p>Logar com o Google</p>
-        </button>
-        <button onClick={loginFacebook}>
-          <img src={Google} alt="" />
-          <p>Logar com o Facebook</p>
         </button>
       </S.Container>
     );
