d04cc29477292eede339dcaedb29216d8b9d2790
Lucas Lima <id.lucas@outlook.com.br>
2022-10-30 01:16:07 -0300

    Environment Variables configuration

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,22 +1,22 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID
+  apiKey: process.env.REACT_APP_VERCEL_ENV_API_KEY,
+  authDomain: process.env.REACT_APP_VERCEL_ENV_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_VERCEL_ENV_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_VERCEL_ENV_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_VERCEL_ENV_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_VERCEL_ENV_APP_ID
 };
 
 console.log (
   {
-    apiKey: process.env.REACT_APP_API_KEY,
-    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-    projectId: process.env.REACT_APP_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_APP_ID
+    apiKey: process.env.REACT_APP_VERCEL_ENV_API_KEY,
+    authDomain: process.env.REACT_APP_VERCEL_ENV_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_VERCEL_ENV_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_VERCEL_ENV_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_VERCEL_ENV_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_VERCEL_ENV_APP_ID
   }
 )
 
